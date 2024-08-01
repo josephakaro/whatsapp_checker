63f8cf362f6fa4816263ade51005bcb774768f96
Joseph Akaro <josephmmakaro@gmail.com>
2024-08-01 21:50:06 +0300

    Added Analytics component for vercel

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Nav from '@components/Nav';
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
 				<Nav />
 				{children}
 			</body>
+			<Analytics />
 		</html>
 	);
 }
