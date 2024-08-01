301e89984accb9346d8ca6c284783d0ba48172f4
Joseph Akaro <josephmmakaro@gmail.com>
2024-08-01 17:45:02 +0300

    Bg

--- app/page.js
+++ app/page.js
@@ -19,42 +19,40 @@ const Home = () => {
 	}, []);
 
 	return (
-		<div className="flex flex-col items-center p-5 sm:max-w-full h-[100vh] bg-slate-100">
-			<div className="bg-green-100 p-4 rounded-lg mt-5 sm:h-[60vh] flex flex-col items-center align-middle gap-4 shadow-md">
-				<h1 className="font-bold text-2xl">
-					WhatsApp <span className="text-green-500">Validator</span>
-				</h1>
-				<p className="text-gray-500">
-					Don't know if a number is a WhatsApp number? WhatsApp
-					validator is the right tool at your grip to validate
-					numbers.
-				</p>
-				<div className="m-5">
-					{status.status ? (
-						<p className="text-green-500">
-							Number is a WhatsApp number
-						</p>
-					) : (
-						<p className="text-red-500">
-							Number is not a WhatsApp number
-						</p>
-					)}
-				</div>
-				<div className="mt-5 flex flex-row items-center justify-center gap-5">
-					<input
-						type="text"
-						className="outline-none border-2 border-green-500 rounded-lg p-2 sm:min-w-96"
-						placeholder="Enter number"
-					/>
-					<motion.button
-						whileHover={{ scale: 1.2 }}
-						whileTap={{ scale: 0.8 }}
-						transition={{ duration: 0.4 }}
-						className="bg-green-500 p-2 rounded-lg text-white w-32"
-					>
-						Check
-					</motion.button>
-				</div>
+		<div className="flex flex-col items-center p-5 max-w-full h-[100vh] bg-slate-200 bg-gradient-to-r from-cyan-200 to-green-300">
+			{' '}
+			<h1 className="font-bold text-2xl">
+				WhatsApp <span className="text-green-500">Validator</span>
+			</h1>
+			<p className="text-gray-500">
+				Don't know if a number is a WhatsApp number? WhatsApp validator
+				is the right tool at your grip to validate numbers.
+			</p>
+			<div className="m-5">
+				{status.status ? (
+					<p className="text-green-500">
+						Number is a WhatsApp number
+					</p>
+				) : (
+					<p className="text-red-500">
+						Number is not a WhatsApp number
+					</p>
+				)}
+			</div>
+			<div className="mt-5 flex flex-row items-center justify-center gap-5">
+				<input
+					type="text"
+					className="outline-none border-2 border-green-500 rounded-lg p-2 sm:min-w-96"
+					placeholder="Enter number"
+				/>
+				<motion.button
+					whileHover={{ scale: 1.2 }}
+					whileTap={{ scale: 0.8 }}
+					transition={{ duration: 0.4 }}
+					className="bg-green-500 p-2 rounded-lg text-white w-32"
+				>
+					Check
+				</motion.button>
 			</div>
 		</div>
 	);
