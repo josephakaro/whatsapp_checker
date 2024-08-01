2ed4de60da0fed27a85a245991d333b57045e8f9
Joseph Akaro <josephmmakaro@gmail.com>
2024-08-01 21:55:58 +0300

    Added SpeedInsight component for vercel

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,7 @@
-import { Inter } from 'next/font/google';
 import './globals.css';
 import Nav from '@components/Nav';
 import { Analytics } from '@vercel/analytics/react';
-
-const inter = Inter({ subsets: ['latin'] });
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export const metadata = {
 	title: 'WhatsApp Checker',
@@ -18,6 +16,7 @@ export default function RootLayout({ children }) {
 				{children}
 			</body>
 			<Analytics />
+			<SpeedInsights />
 		</html>
 	);
 }
